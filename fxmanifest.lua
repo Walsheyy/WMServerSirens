96a77712b7a65ca551bae5510747d75b5722fc7c
Walshey <james.walsh07@icloud.com>
2020-08-05 21:11:21 +0100

    Initial Release.

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'Walshey & Marcus'
+description 'Server Sided Sirens'
+version '1.0.0'
+
+files { 
+	"dlc_wmsirens/sirenpack_one.awc",
+	"data/wmsirens_sounds.dat54.nametable",
+	"data/wmsirens_sounds.dat54.rel"
+}
+
+data_file "AUDIO_WAVEPACK" "dlc_wmsirens"
+data_file "AUDIO_SOUNDDATA" "data/wmsirens_sounds.dat"
+
+-- Join our discord servers https://discord.gg/uVuW5Am & https://discord.gg/uVuW5Am
